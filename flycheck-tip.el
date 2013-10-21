47116b5a801267ae032f1c7e0726e14ee87e5a6b
Yuta Yamada <cokesboy@gmail.com>
2013-10-20 22:46:08 -0400

    Add comments

--- flycheck-tip.el
+++ flycheck-tip.el
@@ -1,3 +1,27 @@
+;;; flycheck-tip.el --- show flycheck's error by popup-tip
+
+;; Copyright (C) 2013 by Yuta Yamada
+
+;; Author: Yuta Yamada <cokesboy"at"gmail.com>
+;; URL: https://github.com/yuutayamada/flycheck-tip
+;; Version: 0.0.1
+;; Package-Requires: ((flycheck "20131020) (popup "20130708"))
+;; Keywords: flycheck
+
+;;; License:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (eval-when-compile (require 'cl))
 (require 'flycheck)
 (require 'popup)
