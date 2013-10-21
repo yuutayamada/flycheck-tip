7784e1c5dfbd08f75fd705d17d8b781e7bd1b0ad
Yuta Yamada <cokesboy@gmail.com>
2013-10-21 14:17:11 -0400

    Modify popup's version specification 20130708 -> 0.5.0

--- flycheck-tip.el
+++ flycheck-tip.el
@@ -5,7 +5,7 @@
 ;; Author: Yuta Yamada <cokesboy"at"gmail.com>
 ;; URL: https://github.com/yuutayamada/flycheck-tip
 ;; Version: 0.0.1
-;; Package-Requires: ((flycheck "0.13") (dash "1.2") (emacs "24.1") (popup "20130708"))
+;; Package-Requires: ((flycheck "0.13") (dash "1.2") (emacs "24.1") (popup "0.5.0"))
 ;; Keywords: flycheck
 
 ;;; License:
