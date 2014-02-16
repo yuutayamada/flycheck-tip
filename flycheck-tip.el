1ac6840760d4334aaa33a8e637b7c497d959db88
Yuta Yamada <cokesboy@gmail.com>
2014-02-16 10:33:26 +0900

    Rename some function from flycheck-tip-* to error-tip-*

--- flycheck-tip.el
+++ flycheck-tip.el
@@ -41,11 +41,6 @@ This variable is true by default."
   "Whether how much delay showing error popup.
 If you set nil to this variable, then do not use delay timer.")
 
-;; INTERNAL VARIABLE
-(defvar flycheck-tip-popup-object nil)
-(defvar flycheck-tip-timer-object nil)
-(defvar flycheck-tip-current-errors nil)
-
 ;; Error status memo
 ;; 0 : err name?
 ;; 1 : buffer
@@ -56,31 +51,48 @@ If you set nil to this variable, then do not use delay timer.")
 ;; 6 : message
 ;; 7 : err type?
 
-;;;###autoload
-(defun flycheck-tip-cycle (&optional reverse)
-  "Move to next error if it's exists.
-If it wasn't exists then move to previous error.
-Move to previous error if REVERSE is non-nil."
-  (interactive)
-  (flycheck-tip-delete-popup)
-  (when flycheck-current-errors
+;; TODO: move error-tip-* functions to error-tip.el
+;; INTERNAL VARIABLE
+(defvar error-tip-popup-object nil)
+(defvar error-tip-timer-object nil)
+(defvar error-tip-current-errors nil)
+
+(defun error-tip-cycle (errors &optional reverse)
+  (error-tip-delete-popup)
+  (when errors
     (lexical-let*
-        ((errors   (flycheck-tip-collect-current-file-errors))
-         (next     (assoc-default :next         errors))
+        ((next     (assoc-default :next         errors))
          (previous (assoc-default :previous     errors))
          (cur-line (assoc-default :current-line errors))
          (jump (lambda (errs)
                  (goto-char (point-min))
                  (forward-line (1- (elt (car errs) 4)))
-                 (setq flycheck-tip-current-errors errs)
+                 (setq error-tip-current-errors errs)
                  (if (null flycheck-tip-timer-delay)
-                     (flycheck-tip-popup-error-message)
+                     (error-tip-popup-error-message (error-tip-get-errors))
                    (flycheck-tip-cancel-timer)
                    (flycheck-tip-register-timer))))
          (target (if (not reverse)
                      (or next previous cur-line)
                    (reverse (or previous next cur-line)))))
       (funcall jump target))))
+
+(defun error-tip-get (err element)
+  (cond
+   ((bound-and-true-p flycheck-mode)
+    (case element
+      (line    (elt err 4))
+      (file    (elt err 3))
+      (message (elt err 6))))))
+
+;;;###autoload
+(defun flycheck-tip-cycle (&optional reverse)
+  "Move to next error if it's exists.
+If it wasn't exists then move to previous error.
+Move to previous error if REVERSE is non-nil."
+  (interactive)
+  (error-tip-cycle
+   (error-tip-collect-current-file-errors flycheck-current-errors) reverse))
 
 ;;;###autoload
 (defun flycheck-tip-cycle-reverse ()
@@ -95,16 +107,15 @@ Move to previous error if REVERSE is non-nil."
       nil
     ad-do-it))
 
-(defun flycheck-tip-collect-current-file-errors ()
-  "Collect errors from `flycheck-current-errors'."
-  (loop with errors = flycheck-current-errors
+(defun error-tip-collect-current-file-errors (errors)
+  "Collect errors from ERRORS."
+  (loop with c-line = (line-number-at-pos (point))
         with next and previous and current-line
         for err in errors
-        for err-line = (elt err 4)
-        for c-line   = (line-number-at-pos (point))
+        for err-line = (error-tip-get err 'line)
         if (and buffer-file-truename ; whether file or buffer
                 (not (equal (expand-file-name buffer-file-truename)
-                            (elt err 3))))
+                            (error-tip-get err 'file))))
         do '() ; skip
         else if (< c-line err-line)
         collect err into next
@@ -116,23 +127,22 @@ Move to previous error if REVERSE is non-nil."
                              (cons :previous     previous)
                              (cons :current-line current-line))))
 
-(defun flycheck-tip-popup-error-message ()
-  "Popup error message(s).
+(defun error-tip-popup-error-message (errors)
+  "Popup error message(s) from ERRORS.
 If there are multiple errors on current line, all current line's errors are
 appeared."
-  (setq flycheck-tip-popup-object
+  (setq error-tip-popup-object
         (popup-tip
-         (format "*%s" (mapconcat 'identity (flycheck-tip-get-errors) "\n*"))
-         :nowait t))
-  (add-hook 'pre-command-hook 'flycheck-tip-delete-popup))
+         (format "*%s" (mapconcat 'identity errors "\n*")) :nowait t))
+  (add-hook 'pre-command-hook 'error-tip-delete-popup))
 
-(defun flycheck-tip-get-errors ()
+(defun error-tip-get-errors ()
   "Get errors."
   (loop with result and fallback
         with current-line = (line-number-at-pos (point))
-        for error in flycheck-tip-current-errors
-        for e-line = (elt error 4)
-        for e-str = (elt error 6)
+        for error in error-tip-current-errors
+        for e-line = (error-tip-get error 'line)
+        for e-str  = (error-tip-get error 'message)
         if (or (equal current-line e-line)
                (and (equal 1 current-line)
                     (equal 0 e-line)))
@@ -144,23 +154,23 @@ appeared."
 
 (defun flycheck-tip-register-timer ()
   "Register timer that show error message."
-  (setq flycheck-tip-timer-object
+  (setq error-tip-timer-object
         (run-with-timer flycheck-tip-timer-delay nil
                         (lambda ()
-                          (flycheck-tip-popup-error-message)))))
+                          (error-tip-popup-error-message (error-tip-get-errors))))))
 
 (defun flycheck-tip-cancel-timer ()
-  "Cancel `flycheck-tip-timer-object'."
-  (when (timerp flycheck-tip-timer-object)
-    (cancel-timer flycheck-tip-timer-object)))
+  "Cancel `error-tip-timer-object'."
+  (when (timerp error-tip-timer-object)
+    (cancel-timer error-tip-timer-object)))
 
-(defun flycheck-tip-delete-popup ()
+(defun error-tip-delete-popup ()
   "Delete popup object."
   (condition-case err
-      (when (popup-live-p flycheck-tip-popup-object)
-        (popup-delete flycheck-tip-popup-object))
+      (when (popup-live-p error-tip-popup-object)
+        (popup-delete error-tip-popup-object))
     (error err))
-  (remove-hook 'pre-command-hook 'flycheck-tip-delete-popup))
+  (remove-hook 'pre-command-hook 'error-tip-delete-popup))
 
 (defun flycheck-tip-display-current-line-error-message (errors)
   "Show current line's ERRORS by popup."
